a0a95d40368be7902479c9172a15b1a3f0d47a2e
jaehyeonngit <jhjhh023@gmail.com>
2025-04-06 23:09:50 +0900

    todolist 복습

--- exercise/src/components/FilterContainer.vue
+++ exercise/src/components/FilterContainer.vue
@@ -1,10 +1,41 @@
-<script></script>
+<script setup>
+import { ref } from 'vue'
+
+//현재 선택된 필터 상태를 관리하는 변수
+const currentFilter = ref('all')
+
+// 필터 변경 이벤트를 부모 컴포넌트에 전달하는 emit 정의
+const emit = defineEmits(['update:filter'])
+
+// 필터 버튼 클릭 핸들러
+function setFilter(filter) {
+  currentFilter.value = filter // 로컬 상태 업데이트트
+  emit('update:filter', filter) // 부모 컴포넌트에 필터 변경 알림
+}
+</script>
 
 <template>
   <div class="filter-container">
-    <button>전체</button>
-    <button>미완료</button>
-    <button>완료</button>
+    <!-- 클릭 시 all 필터 적용
+     현재 필터가 all 이면 버튼에 클래스 적용-->
+    <button
+      @click="setFilter('all')"
+      :class="{ active: currentFilter === 'all' }"
+    >
+      전체
+    </button>
+    <button
+      @click="setFilter('active')"
+      :class="{ active: currentFilter === 'active' }"
+    >
+      미완료
+    </button>
+    <button
+      @click="setFilter('completed')"
+      :class="{ active: currentFilter === 'completed' }"
+    >
+      완료
+    </button>
   </div>
 </template>
 
--- exercise/src/components/SummaryContainer.vue
+++ exercise/src/components/SummaryContainer.vue
@@ -1,10 +1,34 @@
-<script></script>
+<script setup>
+// props 정의
+defineProps({
+  activeCount: {
+    type: Number,
+    default: 0,
+  },
+  completedCount: {
+    type: Number,
+    default: 0,
+  },
+})
+// 이벤트 정의
+const emit = defineEmits('delete-completed')
+
+// 완료된 항목 삭제 이벤트 발생
+function handleDeleteCompleted() {
+  if (!confirm('정말 삭제하시겠습니까?')) return emit('deleted-completed')
+}
+</script>
 
 <template>
+  <!--  등록된 리스트가 있다면 보여지도록 처리리 -->
   <div class="todo-summary">
-    <p><span>미완료항목</span><span>0</span></p>
-    <p><span>완료항목</span><span>0</span></p>
-    <button>완료된 항목 삭제</button>
+    <p>
+      <span>미완료항목</span><span>{{ activeCount }}</span>
+    </p>
+    <p>
+      <span>완료항목</span><span>{{ completedCount }}</span>
+    </p>
+    <button @click="handleDeleteCompleted">완료된 항목 삭제</button>
   </div>
 </template>
 
@@ -33,5 +57,10 @@
 }
 .todo-summary button {
   background-color: var(--blue-dark);
+}
+
+.todo-summary button:disabled {
+  opacity: 0.5;
+  cursor: not-allowed;
 }
 </style>
